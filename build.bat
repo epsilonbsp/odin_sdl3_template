2c19cf289e6da197142bc5c31032f3bac38e1ee3
epsilonbsp <deltasampler@gmail.com>
2026-03-08 15:00:41 +0200

    update build scripts

--- build.bat
+++ build.bat
@@ -1,21 +1,23 @@
 @echo off
 setlocal EnableDelayedExpansion
 
+set BUILD_DIR=build
+set OUTPUT_DIR=%BUILD_DIR%\output
+set VENDOR_DIR=%BUILD_DIR%\vendor
+
 set EXE_NAME=odin_sdl3_template.exe
 set SDL_VERSION=release-3.4.2
 
-if not exist build mkdir build
-if not exist build\output mkdir build\output
-if not exist build\vendor mkdir build\vendor
+if not exist %BUILD_DIR% mkdir %BUILD_DIR%
+if not exist %OUTPUT_DIR% mkdir %OUTPUT_DIR%
+if not exist %VENDOR_DIR% mkdir %VENDOR_DIR%
 
 cls
 
 if "%~1" == "get-sdl3" (
-    curl -L -o build\output\SDL3.dll https://raw.githubusercontent.com/odin-lang/Odin/master/vendor/sdl3/SDL3.dll
-    curl -L -o build\output\SDL3.lib https://raw.githubusercontent.com/odin-lang/Odin/master/vendor/sdl3/SDL3.lib
-)
-
-if "%~1" == "build-sdl3" (
+    curl -L -o %OUTPUT_DIR%\SDL3.dll https://raw.githubusercontent.com/odin-lang/Odin/master/vendor/sdl3/SDL3.dll
+    curl -L -o %OUTPUT_DIR%\SDL3.lib https://raw.githubusercontent.com/odin-lang/Odin/master/vendor/sdl3/SDL3.lib
+) else if "%~1" == "build-sdl3" (
     for /f "tokens=*" %%i in ('"C:\Program Files (x86)\Microsoft Visual Studio\Installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath') do set VS=%%i
 
     if "!VS!" equ "" (
@@ -23,7 +25,7 @@ if "%~1" == "build-sdl3" (
         exit /b 1
     )
 
-    cd build\vendor
+    cd %VENDOR_DIR%
 
     if not exist sld (
         git clone https://github.com/libsdl-org/SDL.git sdl
@@ -37,22 +39,14 @@ if "%~1" == "build-sdl3" (
     cmake --build build --config Release
 
     cd ..\..
-    copy /Y "vendor\sdl\build\Release\SDL3.dll" "output\SDL3.dll"
-    copy /Y "vendor\sdl\build\Release\SDL3.lib" "output\SDL3.lib"
-)
-
-if "%~1" == "build" (
-    odin build source -out:build\output\%EXE_NAME%
-)
-
-if "%~1" == "run" (
-    odin run source -out:build\output\%EXE_NAME%
-)
-
-if "%~1" == "build-debug" (
-    odin build source -out:build\output\%EXE_NAME% -debug
-)
-
-if "%~1" == "run-debug" (
-    odin run source -out:build\output\%EXE_NAME% -debug
+    copy /Y "%VENDOR_DIR%\sdl\build\Release\SDL3.dll" "%OUTPUT_DIR%\SDL3.dll"
+    copy /Y "%VENDOR_DIR%\sdl\build\Release\SDL3.lib" "%OUTPUT_DIR%\SDL3.lib"
+) else if "%~1" == "build" (
+    odin build source -out:%OUTPUT_DIR%\%EXE_NAME%
+) else if "%~1" == "run" (
+    odin run source -out:%OUTPUT_DIR%\%EXE_NAME%
+) else if "%~1" == "build-debug" (
+    odin build source -out:%OUTPUT_DIR%\%EXE_NAME% -debug
+) else if "%~1" == "run-debug" (
+    odin run source -out:%OUTPUT_DIR%\%EXE_NAME% -debug
 )
--- build.sh
+++ build.sh
@@ -1,18 +1,22 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 set -e
+
+BUILD_DIR="build"
+OUTPUT_DIR="$BUILD_DIR/output"
+VENDOR_DIR="$BUILD_DIR/vendor"
 
 EXE_NAME="odin_sdl3_template"
 SDL_VERSION="release-3.4.2"
 
-mkdir -p build
-mkdir -p build/output
-mkdir -p build/vendor
+mkdir -p "$BUILD_DIR"
+mkdir -p "$OUTPUT_DIR"
+mkdir -p "$VENDOR_DIR"
 
 clear
 
 if [ "$1" = "build-sdl3" ]; then
-    cd build/vendor
+    cd "$VENDOR_DIR"
 
     if [ ! -d sdl ]; then
         [ -d sdl ] || git clone https://github.com/libsdl-org/SDL.git sdl
@@ -25,20 +29,12 @@ if [ "$1" = "build-sdl3" ]; then
     cmake --build build
     sudo cmake --install build
     sudo ldconfig
-fi
-
-if [ "$1" = "build" ]; then
-    odin build source -out:build/output/"$EXE_NAME"
-fi
-
-if [ "$1" = "run" ]; then
-    odin run source -out:build/output/"$EXE_NAME"
-fi
-
-if [ "$1" = "build-debug" ]; then
-    odin build source -out:build/output/"$EXE_NAME" -debug
-fi
-
-if [ "$1" = "run-debug" ]; then
-    odin run source -out:build/output/"$EXE_NAME" -debug
+elif [ "$1" = "build" ]; then
+    odin build source -out:"$OUTPUT_DIR/$EXE_NAME"
+elif [ "$1" = "run" ]; then
+    odin run source -out:"$OUTPUT_DIR/$EXE_NAME"
+elif [ "$1" = "build-debug" ]; then
+    odin build source -out:"$OUTPUT_DIR/$EXE_NAME" -debug
+elif [ "$1" = "run-debug" ]; then
+    odin run source -out:"$OUTPUT_DIR/$EXE_NAME" -debug
 fi
